8c9da98641aff328e03b0dd32166fdd942565557
Elor-Itz <elor2200@gmail.com>
2025-04-23 11:54:49 +0300

    Updated text

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,23 @@
+import { Link } from "react-router-dom";
+
 function Home() {
-    return (
-      <section className="p-8">
-        <h1 className="text-4xl font-bold mb-4">Welcome to My Portfolio</h1>
-        <p className="text-lg">
-          Hi, I'm a passionate developer. Explore my projects, learn more about me, or get in touch!
-        </p>
-      </section>
-    );
-  }
-  
-  export default Home;
+  return (
+    <section className="p-8">
+      <h1 className="text-4xl font-bold mb-4">Welcome to My Portfolio</h1>
+      <p className="text-lg mb-6">
+        Hello! I'm Elor Itzkovitz, an aspiring software developer with a passion
+        for building modern, scalable, and user-friendly applications. Take a look at my
+        projects to see how I bring my ideas to life, learn more about me, or check
+        out my contact information!
+      </p>
+      <Link
+        to="/projects"
+        className="inline-block bg-blue-600 text-white px-6 py-3 rounded-lg shadow-md hover:bg-blue-700 transition duration-300"
+      >
+        Check out my projects
+      </Link>
+    </section>
+  );
+}
+
+export default Home;
