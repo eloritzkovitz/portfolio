6b068d55867a1c2664d4c0182d0e186cc1ab31c3
Elor-Itz <elor2200@gmail.com>
2025-04-24 16:02:00 +0300

    Fixed menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -49,32 +49,32 @@ function Navbar() {
 
         {/* Menu */}
         <ul
-          className={`flex flex-col md:flex-row md:space-x-8 absolute md:static top-16 left-0 w-full md:w-auto bg-gray-800 md:bg-transparent transition-all duration-300 ${
-            isMenuOpen ? "block" : "hidden md:flex"
-          }`}
+          className={`flex flex-col md:flex-row md:space-x-8 fixed md:static top-0 right-0 h-full w-1/2 md:w-auto bg-white md:bg-transparent text-black md:text-white transition-all duration-300 ${
+            isMenuOpen ? "translate-x-0" : "translate-x-full"
+          } md:translate-x-0`}
         >
-          <li className="border-b md:border-none">
+          <li>
             <Link
               to="/"
-              className="flex items-center space-x-2 px-4 py-2 hover:text-gray-300"
+              className="flex items-center space-x-2 px-4 py-4 hover:bg-gray-100"
               onClick={() => setIsMenuOpen(false)}
             >
               <FaHome /> <span>Home</span>
             </Link>
           </li>
-          <li className="border-b md:border-none">
+          <li>
             <Link
               to="/about"
-              className="flex items-center space-x-2 px-4 py-2 hover:text-gray-300"
+              className="flex items-center space-x-2 px-4 py-4 hover:bg-gray-100"
               onClick={() => setIsMenuOpen(false)}
             >
               <FaUser /> <span>About</span>
             </Link>
           </li>
-          <li className="border-b md:border-none">
+          <li>
             <Link
               to="/projects"
-              className="flex items-center space-x-2 px-4 py-2 hover:text-gray-300"
+              className="flex items-center space-x-2 px-4 py-4 hover:bg-gray-100"
               onClick={() => setIsMenuOpen(false)}
             >
               <FaProjectDiagram /> <span>Projects</span>
@@ -83,7 +83,7 @@ function Navbar() {
           <li>
             <Link
               to="/contact"
-              className="flex items-center space-x-2 px-4 py-2 hover:text-gray-300"
+              className="flex items-center space-x-2 px-4 py-4 hover:bg-gray-100"
               onClick={() => setIsMenuOpen(false)}
             >
               <FaEnvelope /> <span>Contact</span>
