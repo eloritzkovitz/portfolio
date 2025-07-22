c8b6c3c9ffbd319329fce42ed62c8dd1cdeabfce
Elor-Itz <elor2200@gmail.com>
2025-07-22 15:13:39 +0300

    Added repository link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,22 @@
 function Footer() {
-    return (
-      <footer className="py-4">
-        <div className="max-w-7xl mx-auto px-4 text-center">
-          <p>&copy; 2025 Elor Itzkovitz. All rights reserved.</p>
-        </div>
-      </footer>
-    );
-  }
-  
-  export default Footer;
+  return (
+    <footer className="py-4">
+      <div className="max-w-7xl mx-auto px-4 text-center">
+        <p>
+          &copy; 2025 Elor Itzkovitz. All rights reserved.
+          <br />
+          <a
+            href="https://github.com/Elor-Itz/portfolio"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-teal-500 hover:text-teal-700 underline"
+          >
+            View this website's source on GitHub
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
