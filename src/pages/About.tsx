01faedf7cd92d0525c7483cf3fd6013dd7a70077
Elor-Itz <elor2200@gmail.com>
2025-07-22 14:59:02 +0300

    Added sections navigator component

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { FaList, FaTh } from "react-icons/fa";
+import SectionsNavigator from "../components/SectionsNavigator";
 import { aboutCards } from "../data/aboutData";
 import skillsData from "../data/skillsData";
 import "../styles/About.css";
@@ -25,19 +26,7 @@ function About() {
   return (
     <div className="relative">
       {/* Sections Navigator */}
-      <nav className="hidden sm:flex flex-col gap-4 fixed left-0 top-32 z-40 px-4">
-        <div className="bg-white rounded-lg shadow-md p-4">
-          {sectionAnchors.map((section) => (
-            <a
-              key={section.id}
-              href={`#${section.id}`}
-              className="block text-teal-500 hover:text-teal-700 hover:bg-transparent font-semibold transition-colors py-2 px-2 rounded hover:bg-gray-100"
-            >
-              {section.label}
-            </a>
-          ))}
-        </div>
-      </nav>
+      <SectionsNavigator sections={sectionAnchors} />
 
       <section
         className="about-section w-full max-w-screen-lg mx-auto px-4 sm:px-8 py-4 sm:py-8"
