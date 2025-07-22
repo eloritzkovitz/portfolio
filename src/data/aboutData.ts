7e97bc3d51b57364cffefb407ce7ba9a942009f7
Elor-Itz <elor2200@gmail.com>
2025-07-22 13:51:32 +0300

    Updated about page

---
+++ src/data/aboutData.ts
@@ -0,0 +1,21 @@
+export const aboutCards = [
+  {
+    title: "Introduction",
+    content: [
+      "Hi! My name is Elor. From the day I remember myself, I have always been fascinated by building and creating things with whatever I had in my hands. Time and curiosity have led me to explore the world of technology and programming, where I could bring together my dreams, ideas, and interests - and shape them into reality.",
+      "I am very passionate about building modern, scalable, and user-friendly applications, as well as learning and adapting to new technologies and frameworks. I am always eager to learn and grow, and I believe that the best way to do that is by constantly challenging ourselves.",
+      "Currently, I am a third-year computer science student, exploring the world of full-stack development, learning and experimenting with various technologies and frameworks, mainly React and Node.js.",
+    ],
+  },
+  {
+    title: "Skills",
+    content: [],
+  },
+  {
+    title: "Hobbies & Interests",
+    content: [
+      "When I'm not coding or studying, I try my best to devote my free time to my long list of hobbies and interests. For example, I really like integrating my love for drawing and graphic design with my projects.",
+      "I also enjoy traveling and exploring new places, photographing architecture, nature, and scenery, as well as spending time with my family.",
+    ],
+  },
+];
