d366dd7e4afc314f44525511800a13ffca19f1a5
eloritzkovitz <elor2200@gmail.com>
2025-08-30 23:33:35 +0300

    Improved responsiveness

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -19,30 +19,30 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
 }) => {
   // Left side content
   const left = (
-    <div className="w-1/2 h-full flex flex-col justify-center p-8">
-      <h2 className="text-4xl sm:text-6xl font-bold text-gray-900 dark:text-white mb-4">
+    <div className="w-full md:w-1/2 h-auto md:h-full flex flex-col justify-center p-6 md:p-8">
+      <h2 className="text-2xl sm:text-4xl md:text-6xl font-bold text-gray-900 dark:text-white mb-2 md:mb-4">
         {name}
       </h2>
-      <p className="text-lg sm:text-2xl text-gray-700 dark:text-gray-200 mb-6">
+      <p className="text-base sm:text-lg md:text-2xl text-gray-700 dark:text-gray-200 mb-4 md:mb-6">
         {description}
       </p>
-      <div className="flex flex-wrap gap-2 mb-8">
+      <div className="flex flex-wrap gap-2 mb-4 md:mb-8">
         {tech.map((t, idx) => (
           <span
             key={idx}
-            className="tech-tag bg-white/10 text-white px-2 py-1 rounded"
+            className="tech-tag bg-gray-200 dark:bg-white/10 text-gray-900 dark:text-white px-2 py-1 rounded"
           >
             {t}
           </span>
         ))}
       </div>
       {/* Link */}
-      <div className="p-4">
+      <div className="pt-2 md:p-4">
         <a
           href={`/projects/${name.toLowerCase().replace(/\s+/g, "-")}`}
           target="_blank"
           rel="noopener noreferrer"
-          className="text-teal-500 hover:text-teal-700 font-semibold text-xl block text-start"
+          className="text-teal-500 hover:text-teal-700 font-semibold text-base md:text-xl block text-start"
         >
           Visit project page →
         </a>
@@ -52,18 +52,18 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
 
   // Right side content
   const right = (
-    <div className="relative w-1/2 h-full flex items-center justify-center">
-      <div className="w-[80%] h-[80%] flex items-center justify-center overflow-hidden rounded-xl">
+    <div className="relative w-full md:w-1/2 h-48 md:h-full flex items-center justify-center">
+      <div className="w-[90%] h-[90%] flex items-center justify-center overflow-hidden">
         <img src={image} alt={name} className="w-full h-full object-contain" />
       </div>
     </div>
   );
 
   return (
-    <div className="flex flex-row w-full h-[80vh] rounded-none overflow-hidden mb-0 shadow-none">
+    <div className="flex flex-col md:flex-row w-full h-auto md:h-[80vh] rounded-none overflow-hidden mb-8 shadow-none">
       <Link
         to={`/projects/${name.toLowerCase().replace(/\s+/g, "-")}`}
-        className="flex flex-row w-full h-full"
+        className="flex flex-col md:flex-row w-full h-full"
       >
         {index % 2 === 0 ? (
           <>
