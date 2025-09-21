4875522fab1a0aa979b6f0aeb316f32abe3d1e4b
eloritzkovitz <elor2200@gmail.com>
2025-09-22 00:12:15 +0300

    Fixed direction

--- src/components/layout/navigation/HamburgerMenu.tsx
+++ src/components/layout/navigation/HamburgerMenu.tsx
@@ -23,20 +23,19 @@ const HamburgerMenu: React.FC<HamburgerMenuProps> = ({
   handleTouchEnd,
   className = "",
 }) => {
-
   return (
     <ul
-      className={`flex flex-row fixed top-0 ${
+      className={`flex flex-col fixed top-0 ${
         isRTL ? "left-0" : "right-0"
       } h-full w-1/2 text-black dark:text-white transition-all duration-300 z-50
-    ${
-      isOpen
-        ? "bg-white translate-x-0"
-        : isRTL
-        ? "-translate-x-full"
-        : "translate-x-full"
-    }
-    ${className}
+  ${
+    isOpen
+      ? "bg-white translate-x-0"
+      : isRTL
+      ? "-translate-x-full"
+      : "translate-x-full"
+  }
+  ${className}
   `}
       onTouchStart={isOpen ? handleTouchStart : undefined}
       onTouchEnd={isOpen ? handleTouchEnd : undefined}
